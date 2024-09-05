129746ea4ce6a0dc24656c3049e7aedd1ec38f36
Евгений <evgeniibelov1401@gmail.com>
2024-09-05 11:51:27 +0300

    start

---
+++ LicenceControl/Modules/Licence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenceControl.Modules
+{
+    public class Licence
+    {
+        public string OrganizationName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime StopDate { get; set; }
+    }
+}
---
+++ LicenceControl/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LicenceControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+                Console.Write("Показать тукущую дату (Enter)");
+                Console.ReadLine();
+                Console.WriteLine($"{currentDate():dd/MM/yyyy}");
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+        static DateTime currentDate()
+        {
+            return DateTime.Now;
+        }
+    }
+}
