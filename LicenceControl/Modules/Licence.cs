6374e72493ad4c3658e6a1d91e3de16cd0c1d62c
Евгений <evgeniibelov1401@gmail.com>
2024-09-09 15:23:42 +0300

    осталось чуть чуть

--- LicenceControl/Modules/Licence.cs
+++ LicenceControl/Modules/Licence.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LicenceControl.Exceptions;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +10,56 @@ namespace LicenceControl.Modules
 {
     public class Licence
     {
-        public string OrganizationName { get; set; }
+        public string Name { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime StopDate { get; set; }
+        public DateTime CurrentDay { get; set; }
+        public bool Check { get; set; }
+
+
+        public Licence(string name, string login, string password, string startDate, string stopDate)
+        {
+            this.Name = name;
+            this.Login = login;
+            this.Password = password;
+            this.StartDate = DateTime.Parse(startDate);
+            this.StopDate = DateTime.Parse(stopDate);
+            this.CurrentDay = this.StartDate;
+        }
+
+        public void CheckLicence()
+        {
+            try
+            {
+                if (DateTime.Now >= this.StopDate || this.CurrentDay >= this.StopDate)
+                {
+                    this.Check = false;
+                    this.CurrentDay = DateTime.Now;
+                    throw new EndLicenceException();
+                }
+                else if (DateTime.Now < this.CurrentDay)
+                {
+                    this.Check = false;
+                    throw new NonCurrentDateException();
+                }               
+                else
+                {
+                    this.Check = true;
+                    this.CurrentDay = DateTime.Now;
+                }
+
+            }
+            catch (EndLicenceException)
+            {
+                Console.WriteLine("Срок действия подписки истек...");
+            }
+            catch (NonCurrentDateException)
+            {
+                Console.WriteLine("Установите актуальную дату...");
+            }
+        }
+
     }
 }
--- LicenceControl/Program.cs
+++ LicenceControl/Program.cs
@@ -1,23 +1,47 @@
-﻿namespace LicenceControl
+﻿using LicenceControl.Modules;
+
+namespace LicenceControl
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var licence1 = new Licence("Рога и копыта", "qwerty1234", "123456789", "01/09/2024", "10/09/2024");
             while (true)
             {
-                DateTime now = DateTime.Now;
-                Console.Write("Показать тукущую дату (Enter)");
+                ShowCurrentDate();
                 Console.ReadLine();
-                Console.WriteLine($"{currentDate():dd/MM/yyyy}");
+
+
+                ShowLicenceInfo(licence1);
 
                 Console.ReadLine();
                 Console.Clear();
             }
         }
-        static DateTime currentDate()
+        static DateTime CurrentDate()
         {
             return DateTime.Now;
+        }
+        static void ShowCurrentDate()
+        {
+            Console.Write("Показать тукущую дату (Enter)");
+            Console.ReadLine();
+            Console.WriteLine($"{CurrentDate():dd/MM/yyyy}");
+        }
+
+        static void ShowLicenceInfo(Licence licence)
+        {
+            Console.WriteLine($"Организация: {licence.Name}\n" +
+                $"Логин: {licence.Login}\nПароль: {licence.Password}");
+            licence.CheckLicence();
+            if (licence.Check)
+            {
+                Console.WriteLine($"Подписка действительна..." +
+                    $"\nДата начала подписки: {licence.StartDate.ToString("dd.MM.yyyy")}" +
+                    $"\nДата окончания подписки: {licence.StopDate.ToString("dd.MM.yyyy")}" +
+                    $"\nДней до окончания подписки: {(licence.StopDate - CurrentDate()).ToString("dd")}");
+            }
         }
     }
 }
